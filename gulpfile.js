3d4b26b68ee37f23f87ab630d928eab54a12e004
김하영 <khy4018@gmail.com>
2022-04-05 17:42:53 +0900

    Create repository

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename")
+
+gulp.task('gulpEjs', function() {
+    return gulp
+        .src([ "./src/html/*", "!" + "./src/html/include/*" ])
+        .pipe(ejs({title: "gulp-ejs"}))
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest('./dist/html'));
+});
+
+
+gulp.task( "default", gulp.parallel("gulpEjs") );
